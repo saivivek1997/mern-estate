f71ca3c94d0a104c4f7b409a120d6cb53e26ba26
saivivek23 <raavisaivivek@gmail.com>
2024-05-12 20:18:42 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import userRouter from "./routes/user.route.js";
 const app = express();
 
 dotenv.config();
@@ -14,3 +15,5 @@ mongoose
 app.listen(3000, () => {
   console.log("Sever is running on port 3000!!!");
 });
+
+app.use("/api/user", userRouter);
