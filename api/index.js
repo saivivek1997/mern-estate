9bb122b1b6964e264da7d857970ebbd9deb81866
saivivek23 <raavisaivivek@gmail.com>
2024-05-12 19:53:54 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 const app = express();
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("connected to db"))
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log("Sever is running on port 3000!!!");
